baaeee5ec16b11fa964bc74ce1ae0fad23cc354e
Angela2389 <angela_2389@hotmail.com>
2016-08-14 10:36:39 +0200

    Seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+johnlegend = Artist.create({ name: 'John Legend'})
+lilwayne = Artist.create({ name: "Lil' Wayne"})
+k3 = Artist.create({ name: 'K3'})
+
+Song.create({ title: 'All of me', artist: johnlegend })
+Song.create({ title: 'Tonight', artist: johnlegend})
+Song.create({ title: 'Ordinary People', artist: johnlegend})
+Song.create({ title: 'A Milli', artist: lilwayne})
+Song.create({ title: 'How to love', artist: lilwayne})
+Song.create({ title: 'Als het binnen regent', artist: k3})
