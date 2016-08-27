f95399a565d8e6e77d743e49b6c37627f5b47d8d
Angela2389 <angela_2389@hotmail.com>
2016-08-27 16:35:51 +0200

    adding cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'angela2389'
+  config.api_key = '281456478821331'
+  config.api_secret = 'jIxuL0TijmnYDiYQKsyE6ijmE90'
+  config.cdn_subdomain = true
+end
