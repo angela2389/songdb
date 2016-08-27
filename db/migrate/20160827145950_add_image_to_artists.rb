ac5149dd124e5a594b112cba82853d27049ea1c3
Angela2389 <angela_2389@hotmail.com>
2016-08-27 17:24:22 +0200

    show page with cloudinary image

--- db/migrate/20160827145950_add_image_to_artists.rb
+++ db/migrate/20160827145950_add_image_to_artists.rb
@@ -1,4 +1,4 @@
-class AddImageToArtist < ActiveRecord::Migration[5.0]
+class AddImageToArtists < ActiveRecord::Migration[5.0]
   def change
     add_column :artists, :image, :string
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-johnlegend = Artist.create({ name: 'John Legend'})
-lilwayne = Artist.create({ name: "Lil' Wayne"})
-k3 = Artist.create({ name: 'K3'})
+Song.delete_all
+Artist.delete_all
+
+johnlegend = Artist.create( name: 'John Legend', remote_image_url: "https://lh5.googleusercontent.com/-sWpAFzY0h64/AAAAAAAAAAI/AAAAAAAAB9w/7h2selUbLbk/s0-c-k-no-ns/photo.jpg")
+lilwayne = Artist.create( name: "Lil' Wayne", remote_image_url: "http://whoisbiography.com/wp-content/uploads/2015/06/url.jpg")
+k3 = Artist.create( name: 'K3', remote_image_url: "https://studio100.com/webshop/be/2469/k3-cd-10000-luchtballonnen.jpg")
 
 Song.create({ title: 'All of me', artist: johnlegend })
 Song.create({ title: 'Tonight', artist: johnlegend})
